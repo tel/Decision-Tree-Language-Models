7b4485b134bae33f6fa8cb35a1c1a41a5e97d87c
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-05-04 02:51:19 -0400

    * Main.hs: Created the other trees. Local style.

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ module Main where
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import qualified Data.Foldable as F
+import  Data.Foldable (foldMap)
 import Data.Ord (comparing)
 import Data.List
 import Text.ParserCombinators.Parsec
@@ -22,7 +22,7 @@ import Agglomeration
 -- EncIx :: (History index, Bitstream index)
 type EncIx = (Int, -- History element index
               Int) -- Within-element bitstream index
-newtype Bitstream = Bitstream (Array EncIx Motion)
+newtype Bitstream = Bitstream { bitstreamArray :: (Array EncIx Motion) }
 
 instance Show Bitstream where
   show (Bitstream arr) = concatMap show (elems arr)
@@ -44,63 +44,91 @@ bitPredicts bs = mapM fn
 -- This datatype represents a binary decision tree that encodes a
 -- question of the form (Split ix l r) ~> "continute to the left
 -- branch if predictor at ix is TLeft and visa versa".
---
--- Leaves contain boolean values which determine whether they can
--- continue growing.
-data QTree = Split EncIx QTree QTree | Leaf Bool deriving (Show, Eq)
-deadLeaf = Leaf False
-liveLeaf = Leaf True
+data QTree = Split EncIx QTree QTree | Leaf deriving (Show, Eq)
 
+qAbove :: Int -> QTree -> [[EncIx]]
+qAbove n tree = reverse $ rep n [tree] []
+    where rep 0 _ lst = lst
+          rep n trees lst = let (lst', trees') = ((: lst) . concat *** (++ trees) . concat) $ 
+                                                 unzip (map trim trees)
+                            in rep (n-1) trees' lst'
+          trim Leaf = ([], [])
+          trim (Split ix l r) = ([ix], [l, r])
 
 -- Splits the data into two sets based on whether each item's
 -- predictor at EncIx is TLeft (fst) or TRight (snd)
-splitDataWith :: EncIx -> EncData -> (EncData, EncData)
-splitDataWith ix = partition fn
+splitDataWith :: EncData -> EncIx -> (EncData, EncData)
+splitDataWith dat ix = partition fn dat
     where fn (_, Bitstream arr) = arr ! ix == TLeft
 
 
--- Computes a sample entropy passed to a given Leaf in a QTree
-entropyAtLeaf :: QTree -> Path -> EncData -> Double
-entropyAtLeaf (Leaf _) (Path []) dat = entropy $ map fst dat
-entropyAtLeaf (Leaf _) (Path (m:_)) _ = error "Path does not lead to leaf."
-entropyAtLeaf (Split ix l r) (Path (m:ms)) dat = 
-    case m of
-      TLeft  -> entropyAtLeaf l (Path ms) (fst $ splitDataWith ix dat)
-      TRight -> entropyAtLeaf r (Path ms) (snd $ splitDataWith ix dat)
+-- unfoldQTree
+-- Build a QTree recursively using the maybeSplit function.
+-- 
+unfoldQTree :: (EncData, EncData)   -- Development and Held Out data
+            -> ((EncData, EncData)  -- Locally available dev and ho data
+                    -> [Int]        -- Admissible question key
+                    -> Maybe EncIx) -- If we'll divide, this is the
+                                    -- split to do it by
+            -> QTree
+unfoldQTree (dev, ho) maybeSplit = rep (dev, ho) (replicate n 0)
+    where ((_, _), (n, _)) = bounds $ bitstreamArray $ snd $ head dev
+          rep (dev, ho) curkey = 
+              case maybeSplit (dev, ho) curkey of
+                Nothing -> Leaf
+                Just ix@(i, j) -> Split ix lsplit rsplit
+                    where lsplit = rep (ldev, lho) newkey
+                          rsplit = rep (rdev, rho) newkey
+                          (ldev, rdev) = splitDataWith dev ix
+                          (lho, rho) = splitDataWith ho ix
+                          newkey = take i curkey ++ [(curkey !! i) + 1] ++ drop (i+1) curkey
 
-                          
--- Splits a leaf adding the particular EncIx constraint to the tree                       
-splitLeaf :: QTree -> Path -> EncIx -> QTree
-splitLeaf (Leaf True) (Path []) ix = Split ix liveLeaf liveLeaf
-splitLeaf (Leaf False) (Path []) ix = error "Cannot split a dead leaf."
-splitLeaf (Leaf _) (Path (m:_)) _ = error "Path does not lead to leaf."
-splitLeaf (Split ixhere l r) (Path (m:ms)) ix = 
-    case m of 
-      TLeft -> Split ixhere (splitLeaf l (Path ms) ix) r
-      TRight -> Split ixhere l (splitLeaf r (Path ms) ix)
-
-        
--- finds the best possible split of EncData given a bitmask of
--- previously asked questions
-bestSplit :: EncData -> [Int] -> (EncIx, Double)
-bestSplit dat mask = maximumBy (comparing snd) splits
-    where splits :: [(EncIx, Double)]
-          splits = map (id &&& (\ix -> splitEntropy (splitDataWith ix dat))) indices
-          indices = zip [0..] mask
-          splitEntropy :: (EncData, EncData) -> Double
-          splitEntropy (as, bs) = let na = fromIntegral $ length as
-                                      nb = fromIntegral $ length bs
-                                      n  = na + nb
-                                  in (na/n) * dataEntropy as + (nb/n) * dataEntropy bs
-          dataEntropy = entropy . map fst
-
--- Builds question masks and data splits along the tree, maps them
--- with the passed function, then returns an accumulation
--- mapPaths :: QTree -> EncData -> (EncData -> [Int] -> a) -> [(a, Path)]
-
--- filter a data set along a path through a QTree
--- treeFilter :: QTree -> Path -> Data -> Data
-
+-- Creates a tree from a given data set
+devPercent = 0.8 :: Float
+crossEntropyCutoff = 0:: Double
+buildTree :: EncData -> QTree
+buildTree dat = unfoldQTree (splitData devPercent dat) evaluator
+    where exampleArr = bitstreamArray $ snd (head dat)
+          realQp = inRange (bounds exampleArr)
+          splitData :: Float -> [a] -> ([a], [a])
+          splitData percentage dat = 
+              splitAt (ceiling $ (fromIntegral $ length dat)*percentage) dat
+          evaluator :: (EncData, EncData) -> [Int] -> Maybe EncIx
+          evaluator (dev0, ho0) key = case permisQ of
+                                        [] -> Nothing
+                                        _  -> case cE0 - cE1 > crossEntropyCutoff of
+                                                -- This scaling might
+                                                -- not be correct. We
+                                                -- might need to
+                                                -- propagate the
+                                                -- current frequency
+                                                -- of this equivalence
+                                                -- class and
+                                                -- divide. Also cE1
+                                                -- seems to be zero'd.
+                                                False -> Nothing
+                                                True -> Just bestQ
+              where permisQ = filter realQp $ zip [0..] key
+                    bestQ = fst $ 
+                            minimumBy (comparing snd) $ 
+                            map (id &&& splitEntropy . splitDataWith dev0) permisQ
+                    splitEntropy (dev1, dev2) = (n1/n) * entropy (map fst dev1) + 
+                                                (n2/n) * entropy (map fst dev2)
+                        where n1 = fromIntegral $ length dev1
+                              n2 = fromIntegral $ length dev2
+                              n  = n1 + n2
+                    crossEntropy ho dev0 = let g = obj_freq (map fst ho0)
+                                               f = obj_freq (map fst dev0)
+                                               fn w = Sum $ -freqOf w g * (log $ freqOf w f)
+                                           in getSum $ foldMap fn alphabet
+                    cE0 = crossEntropy ho0 dev0
+                    (bdev1, bdev2) = splitDataWith dev0 bestQ
+                    (bho1, bho2) = splitDataWith ho0 bestQ
+                    n1 = fromIntegral $ length bho1
+                    n2 = fromIntegral $ length bho2
+                    n  = n1 + n2
+                    cE1 = (n1/n) * (crossEntropy bho1 bdev1) +
+                          (n2/n) * (crossEntropy bho2 bdev2)
 
 --
 -- Test data
@@ -111,16 +139,11 @@ tree1 = fmap (head . Set.toList) $ read "((  (e (u (o (i a))))) (h ((n (l (x r))
 bsenc :: Map.Map Char Path
 bsenc = BT.bsEncode tree1
 
-splitData :: Double -> [a] -> ([a], [a])
-splitData percentage dat = splitAt (ceiling $ (fromIntegral n)*percentage) dat
-  where n = length dat
-
 -- Get the development/validation data sets        
-(dev, cross) = splitData 0.8 train
 fourgram :: [d] -> [(d, d, d, d)]
 fourgram os = zip4 os (tail os) (tail $ tail os) (tail $ tail $ tail os)
 
 -- Split the data up into fourgrams
-dev4g, cross4g :: EncData
-Just dev4g = bitPredicts bsenc $ fourgram dev
-Just cross4g = bitPredicts bsenc $ fourgram cross
+Just train4g = bitPredicts bsenc $ fourgram train
+
+t = buildTree train4g
