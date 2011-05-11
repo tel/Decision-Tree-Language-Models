369f73ee0a24e5adf32bd527797ad828a9f68c6a
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-05-11 19:44:46 -0400

    * Util.hs: Refactored Util.hs to include some printing functions

---
+++ Util.hs
@@ -0,0 +1,21 @@
+module Util where
+
+import DTree
+
+dTreeJSON :: Show a => DTree s a -> (s -> String) -> String
+dTreeJSON (Leaf x) _ = "var dtree = " ++ show x
+dTreeJSON tr showq = "var dtree = " ++ printDTree' tr
+    where
+      printDTree' (Leaf _) = "1"
+      printDTree' (Branch q (Leaf _) (Leaf _)) = 
+          "{name: '" ++ showq q ++ "'}"
+      printDTree' (Branch q l (Leaf _)) = 
+          "{name: '" ++ showq q ++ "', left: " ++ printDTree' l ++ "}"
+      printDTree' (Branch q (Leaf _) r) = 
+          "{name: '" ++ showq  q ++ "', right: " ++ printDTree' r ++ "}"
+      printDTree' (Branch q l r) = 
+          "{name: '" ++ showq q ++ "', left: " ++ printDTree' l ++ 
+                         ", right: " ++ printDTree' r ++ "}"
+
+showBSQ :: (Int, Int) -> String
+showBSQ (i, j) = show (3-i) ++ ":" ++ show j
